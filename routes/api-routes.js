133f7efc4505c68704a2a664c494e1a7a3135bd7
Ayse <aysendl@gmail.com>
2020-07-09 13:45:07 -0700

    fixing lint errors

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,38 +1,38 @@
-const router =require("express").Router();
-const dev ={
-    status:"In development"
-}
+const router = require("express").Router();
+const dev = {
+  status: "In development"
+};
 
 // Placeholder API routes
 
-router.get("/api/items", function(req, res){
-    console.log("Read Items");
-    res.json(dev);
-});
-    
-router.post("/api/items", function(req, res){
-    console.log("Create Items");
-    res.json(dev);
-});
-    
-router.delete("/api/items/:id", function(req, res){
-    console.log("Delete Items");
-    res.json(dev);
+router.get("/api/items", (req, res) => {
+  console.log("Read Items");
+  res.json(dev);
 });
 
-router.get("/api/categories", function(req, res){
-    console.log("Read categories");
-    res.json(dev);
+router.post("/api/items", (req, res) => {
+  console.log("Create Items");
+  res.json(dev);
 });
-    
-router.post("/api/categories", function(req, res){
-    console.log("Create categories");
-    res.json(dev);
+
+router.delete("/api/items/:id", (req, res) => {
+  console.log("Delete Items");
+  res.json(dev);
 });
-    
-router.delete("/api/categories/:id", function(req, res){
-    console.log("Delete categories");
-    res.json(dev);
+
+router.get("/api/categories", (req, res) => {
+  console.log("Read categories");
+  res.json(dev);
 });
-    
+
+router.post("/api/categories", (req, res) => {
+  console.log("Create categories");
+  res.json(dev);
+});
+
+router.delete("/api/categories/:id", (req, res) => {
+  console.log("Delete categories");
+  res.json(dev);
+});
+
 module.exports = router;
