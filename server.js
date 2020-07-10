6632781b1e61ba6bbdd115f30264b628dd553a88
Ha Phan <haphan64@outlook.com>
2020-07-09 21:40:10 -0700

    remove index.html, update for handlebar

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const apiRoutes = require("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(apiRoutes);
+app.use(htmlRoutes);
 
 // require("routeTBD")(app);
 
