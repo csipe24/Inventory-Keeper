d2d9cb4585afc24c2dd4a395f04ad346418be6dc
ICVRXS <calebhay@rocketmail.com>
2020-07-09 19:24:24 -0700

    added html routes to server.js. I'm inclined to believe the controller is unnecessary since we already have the routes set up and required in the server.js file, but I may be wrong.

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const apiRoutes = require("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(apiRoutes);
-
-// require("routeTBD")(app);
+app.use(htmlRoutes);
 
 db.sequelize.sync({}).then(() => {
   app.listen(PORT, () => {
